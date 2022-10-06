b511edb0d59eaa1186fbf33dc8cb0d016a406743
igr-0k <igor.01101110@gmail.com>
2022-10-06 18:06:22 +0300

    Task#1 - done!

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,24 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
-console.log(galleryItems);
+
+let itemsGallery = galleryItems;
+
+const galleryTemplate = itemsGallery
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt= "${description}" />
+    </a>`;
+  })
+  .join('');
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', galleryTemplate);
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// console.log(SimpleLightbox);
+// console.log(galleryItems);
